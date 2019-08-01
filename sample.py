af78f0151e95bcf6f5d8a99a666552e21ea9f670
sim2egor <sim2egor@outlook.com>
2019-08-01 09:47:40 +0300

    delele paho source

--- sample.py
+++ sample.py
@@ -52,7 +52,7 @@ mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
 # mqttc.on_log = on_log
-mqttc.connect("192.168.1.16", 1883, 60)
+mqttc.connect("localhost", 1883, 60)
 mqttc.subscribe("#", 0)
 
 mqttc.loop_forever()

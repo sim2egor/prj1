d6f8cab89ba0be937c04fbd81212dc0701306e49
sim2egor <sim2egor@outlook.com>
2019-08-01 23:13:19 +0300

    add mqtt send

--- Keyboard_sample.py
+++ Keyboard_sample.py
@@ -1,14 +1,24 @@
 import curses
+import curses
+import paho.mqtt.publish as publish
+
+publish.single("paho/test/single", "up", hostname="192.168.1.46")
+
+
 def up():
+    publish.single("paho/test/single", "up", hostname="192.168.1.46")
     print("up")
 
 def backward():
+    publish.single("paho/test/single", "backward", hostname="192.168.1.46")
     print("bakward")
 
 def left():
+    publish.single("paho/test/single", "left", hostname="192.168.1.46")
     print("left")
 
 def right():
+    publish.single("paho/test/single", "right", hostname="192.168.1.46")
     print("right")
 
 actions = {
@@ -38,6 +48,7 @@ def main(window):
             while next_key == key:
                 next_key = window.getch()
             # KEY RELEASED
+            publish.single("paho/test/single", "stop", hostname="192.168.1.46")
             print('stop()')
 
 curses.wrapper(main)
